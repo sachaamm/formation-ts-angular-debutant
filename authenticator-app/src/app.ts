7925537d605b5d73873a656b445b3ef792c2c5c2
Alexandre Amiel <sachaamm@gmail.com>
2023-05-26 09:16:20 +0200

    Autenticator App WIP

--- authenticator-app/src/app.ts
+++ authenticator-app/src/app.ts
@@ -7,16 +7,33 @@ import * as fs from "fs";
 import { User } from "./model/user.model";
 import { Role } from "./model/role.enum";
 
+const cors = require('cors');
+
 const app: express.Application = express();
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
 
-app.route('/api/login')
-    .post(loginRoute);
 
-const RSA_PRIVATE_KEY = fs.readFileSync('./keys/private.key');
+// app.route('/login')
+//     .post(loginRoute);
 
-export function loginRoute(req: Request, res: Response) {
+app.get('/', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.send({
+        message: 'Hello'
+    });
+});
+
+app.post('/login', (req, res) => {
+
+    res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.set('Access-Control-Allow-Credentials', 'True');
+
+    console.log('login Route');
 
     const email = req.body.email;
     const password = req.body.password;
@@ -30,12 +47,28 @@ export function loginRoute(req: Request, res: Response) {
             subject: userId
         });
 
+        console.log('Valid mail and password ');
+        console.log(jwtBearerToken)
+
+        res.send(jwtBearerToken);
         // send the JWT back to the user
         // TODO - multiple options available                              
     } else {
         // send status 401 Unauthorized
         res.sendStatus(401);
     }
+});
+
+const port = 4205;
+
+app.listen(port, () => {
+    console.log('listening on port ' + port);
+});
+
+const RSA_PRIVATE_KEY = fs.readFileSync('./keys/key.pem');
+
+export function loginRoute(req: Request, res: Response) {
+
 }
 
 function validateEmailAndPassword(): boolean {
@@ -47,6 +80,7 @@ const users: User[] = [
     { id: 2, email: 'myMail2@mail.com', password: 'myPassword2', role: Role.AdminRole }
 ];
 
-function findUserIdForEmail(email: string): number {
-    throw new Error("Function not implemented.");
+function findUserIdForEmail(email: string): string {
+    // throw new Error("Function not implemented.");
+    return "hello";
 }
